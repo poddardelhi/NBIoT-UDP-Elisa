d2cf9797c11413021f686e1aa0b37e51166a68b9
nishant <poddardelhi@gmail.com>
2020-03-26 07:38:44 +0200

    Initial commit

---
+++ UART_Timer_L476RG_BG96_Shield/MDK-ARM/RTE/_UART_L476RG_BG96_Shield/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'UART_L476RG_BG96_Shield' 
+ * Target:  'UART_L476RG_BG96_Shield' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
